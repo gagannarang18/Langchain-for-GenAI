0d8a5c514ad93bf5c0c4332e55468cf5c18dcbb1
Gagan Narang <gagannarang0804@gmail.com>
2025-05-03 15:24:55 +0530

    implemented basic chain concept

---
+++ Chains/chains.py
@@ -0,0 +1,19 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt= PromptTemplate(
+    template="What is the capital of {country}?",
+    input_variables=["country"]
+)
+
+model = ChatGroq(model="llama-3.1-8b-instant")
+
+parser=StrOutputParser()
+
+chain=prompt | model | parser
+result = chain.invoke({"country": "India"})
+print(result)
