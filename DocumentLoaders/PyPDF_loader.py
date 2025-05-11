36de4c4050537c456dd133c7d7f4668b1c12935d
Gagan Narang <gagannarang0804@gmail.com>
2025-05-11 20:37:53 +0530

    implemented pypdf loader

--- DocumentLoaders/PyPDF_loader.py
+++ DocumentLoaders/PyPDF_loader.py
@@ -1,5 +1,11 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_groq import ChatGroq
 
-loader=PyPDFLoader("sample.pdf")
+loader=PyPDFLoader("dl-curriculum.pdf")
+
 docs = loader.load()    
+
+print(len(docs)) # Number of documents loaded
+
+print(docs[0].page_content) # Content of the first document
+print(docs[0].metadata) # Metadata of the first document
