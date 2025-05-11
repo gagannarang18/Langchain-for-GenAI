dd0278bf35dd8bce649550b7c35583cd3034a26f
Gagan Narang <gagannarang0804@gmail.com>
2025-05-11 20:12:55 +0530

    implemented textloader

--- DocumentLoaders/text_loader.py
+++ DocumentLoaders/text_loader.py
@@ -1,7 +1,28 @@
 from langchain_community.document_loaders import TextLoader 
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatGroq(model="llama-3.1-8b-instant")
+parser=StrOutputParser()
+
+prompt= PromptTemplate(
+    "Write a summary of the following poem: {poem}",
+    input_variables=["poem"]
+)
+
 
 loader=TextLoader('cricket.txt', encoding='utf-8')
 
 docs = loader.load()
 
-print(type(docs))
+chain= prompt | model | parser
+
+result = chain.invoke({"poem": docs[0].page_content})
+print(result)
+
+
+
