0246781c6f28d3d516f7dcd692b675e1c5349f4b
Gagan Narang <gagannarang0804@gmail.com>
2025-05-11 21:11:17 +0530

    added a note

--- DocumentLoaders/csv_loader.py
+++ DocumentLoaders/csv_loader.py
@@ -5,3 +5,6 @@ loader=CSVLoader("Social_Network_Ads.csv")
 docs = loader.load()
 
 print(len(docs)) 
+
+#for every row , there will be a document created.
+print(docs[0].page_content) # Content of the first document
