f44616fb327058b70cd8420868a321c30ade2df6
Gagan Narang <gagannarang0804@gmail.com>
2025-06-17 19:50:24 +0530

    Recursive text splitter

--- TextSplitters/Recursive.py
+++ TextSplitters/Recursive.py
@@ -1,18 +1,17 @@
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 loaader= PyPDFLoader("dl-curriculum.pdf")
 
 docs= loaader.load()
 
-splitter= CharacterTextSplitter(
-    separator="",
-    chunk_size=100,
+splitter= RecursiveCharacterTextSplitter(
+    chunk_size=200,
     chunk_overlap=0
 )
 
 result=splitter.split_documents(docs)
 
-print(result[0])
+print(result[4])
 
 ##chunk overlap helps to avoid losing context between chunks
